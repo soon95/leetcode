8aba489829e538ef686e64e2e7198be46990f49e
Leon Song <songliu0608@qq.com>
2019-05-25 11:43:54 +0800

    修改目录结构

---
+++ src/Interval.java
@@ -0,0 +1,16 @@
+public class Interval {
+
+    int start;
+    int end;
+
+    Interval(int left,int right){
+        this.start=right;
+        this.end=end;
+    }
+
+    public void print(){
+        System.out.printf("["+start+","+end+"]");
+    }
+
+
+}
